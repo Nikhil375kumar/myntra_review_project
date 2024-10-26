d125132a8a89ae298ab5376fd5071af3308a513d
Nikhil Kumar <nikhil375kumar@gmail.com>
2024-10-26 15:41:28 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='NIKHIL_KUMAR',
+    author_email='nikhil375kumar@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
